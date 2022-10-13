46bd34ccdf8567b33c0c1f7e933c67ce77e27b68
sac <358329098@qq.com>
2022-10-13 21:02:00 +0800

    Nacos配置统一的配置管理

--- cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,10 +1,13 @@
 package cn.itcast.user.web;
 
-import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
@@ -14,14 +17,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    /**
-     * 路径： /user/110
-     *
-     * @param id 用户id
-     * @return 用户
-     */
-    @GetMapping("/{id}")
-    public User queryById(@PathVariable("id") Long id) {
-        return userService.queryById(id);
+    @Value("${pattern.dateformat}")//占位符形式拿值
+    private String dateformat;
+
+    @GetMapping("now")
+    public String now() {
+        log.info("dateformat:{}", dateformat);
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
     }
 }
