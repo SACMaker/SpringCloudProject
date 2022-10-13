46f1df54125d3b22a03dd68a7d57cc29a5f351aa
sac <358329098@qq.com>
2022-10-13 21:32:37 +0800

    Nacos配置热更新和配置共享

--- cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,9 +1,11 @@
 package cn.itcast.user.web;
 
+import cn.itcast.user.config.PatternProperties;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -12,17 +14,32 @@ import java.time.format.DateTimeFormatter;
 @Slf4j
 @RestController
 @RequestMapping("/user")
+@RefreshScope
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @Value("${pattern.dateformat}")//占位符形式拿值
+/*    @Value("${pattern.dateformat}")//占位符形式拿值
     private String dateformat;
 
     @GetMapping("now")
     public String now() {
         log.info("dateformat:{}", dateformat);
         return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+    }*/
+
+    @Autowired
+    private PatternProperties patternProperties;
+
+    @GetMapping("/now")
+    public String now() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(patternProperties.getDateformat()));
     }
+
+    @GetMapping("prop")
+    public PatternProperties prop() {
+        return patternProperties;
+    }
+
 }
