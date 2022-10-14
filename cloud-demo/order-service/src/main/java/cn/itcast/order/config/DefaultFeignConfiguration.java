a2d77f97f76cceed8097ed4d3507fde5b599fcdc
sac <358329098@qq.com>
2022-10-14 13:52:17 +0800

    添加OpenFeign优化之http连接池httpClient

--- cloud-demo/order-service/src/main/java/cn/itcast/order/config/DefaultFeignConfiguration.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/config/DefaultFeignConfiguration.java
@@ -9,6 +9,6 @@ import org.springframework.context.annotation.Bean;
 public class DefaultFeignConfiguration {
     @Bean
     public Logger.Level feignLogLevel() {
-        return Logger.Level.BASIC; // 日志级别为BASIC
+        return Logger.Level.BASIC; // 日志级别为BASIC,优化优先用这个
     }
 }
