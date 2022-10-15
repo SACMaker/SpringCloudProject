e310df324446c52473f97c8188270d926f3d21ee
sac <358329098@qq.com>
2022-10-15 16:39:06 +0800

    spring-amqp 实现WorkQueue工作消息队列模型

--- mq-demo/publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
+++ mq-demo/publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
@@ -14,6 +14,10 @@ public class SpringAmqpTest {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    /**
+     *  BasicQueue 基本消息队列模型
+     *  一个提供者一个消费者
+     */
     @Test
     public void testSimpleQueue() {
         // 队列名称
@@ -22,5 +26,23 @@ public class SpringAmqpTest {
         String message = "hello, spring amqp!";
         // 发送消息
         rabbitTemplate.convertAndSend(queueName, message);
+    }
+
+    /**
+     * WorkQueue 工作消息队列模型
+     * 向队列中不停发送消息，模拟消息堆积
+     * 一个提供者多个消费者
+     */
+    @Test
+    public void testWorkQueue() throws InterruptedException {
+        // 队列名称
+        String queueName = "simple.queue";
+        // 消息
+        String message = "hello, message_";
+        for (int i = 0; i < 50; i++) {
+            // 发送消息
+            rabbitTemplate.convertAndSend(queueName, message + i);
+            Thread.sleep(20);
+        }
     }
 }
